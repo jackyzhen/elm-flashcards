ea0e501006f2b29785614212bf16f0009444c88d
Jacky Zhen <jacky.s.zhen@gmail.com>
2017-01-19 16:56:22 +1300

    following an elm webpack starter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Pokerface
 A scrum poker app made with elm.
+Bootstrapped via [https://github.com/moarwick/elm-webpack-starter](https://github.com/moarwick/elm-webpack-starter)
---
+++ src/elm/Main.elm
@@ -0,0 +1,66 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+
+main : Program Never Model Msg
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+
+type alias Model =
+  { content: String,
+    points: Int }
+
+
+model : Model
+model =
+  { content = "", points = 0 }
+
+-- UPDATE
+
+type Msg
+  = Change String
+  | Point Int
+
+update: Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+
+    Point newPoints ->
+      { model| points = newPoints}
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [
+      div[] [ img [ src "static/img/elm.jpg", style styles.img ] [] ]
+    ,  input [ placeholder "Story to point...", onInput Change] []
+    , button [ onClick (Point 0) ] [ text "0" ]
+    , button [ onClick (Point 1) ] [ text "1" ]
+    , button [ onClick (Point 2) ] [ text "2" ]
+    , button [ onClick (Point 3) ] [ text "3" ]
+    , button [ onClick (Point 5) ] [ text "5" ]
+    , button [ onClick (Point 8) ] [ text "8" ]
+    , button [ onClick (Point 13) ] [ text "13" ]
+    , button [ onClick (Point 20) ] [ text "20" ]
+    , button [ onClick (Point 40) ] [ text "40" ]
+    , button [ onClick (Point 100) ] [ text "100" ]
+    , div [] [ text ("Story: " ++ .content model) ] -- trying weird record property access syntax
+    , div [] [ text ("Points: " ++ toString model.points) ]
+    ]
+
+-- INLINE CSS STYLES
+styles : { img : List ( String, String ) }
+styles =
+  {
+    img =
+      [ ( "width", "33%" )
+      , ( "border", "4px solid #337AB7")
+      ]
+  }
